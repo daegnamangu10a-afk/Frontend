7b445a51cb67a0b9f0a5a6434ad8e85787f62035
daegnamangu10a-afk <daegnamangu10a@gmail.com>
2026-09-23 22:37:14 +0530

    Updated Backend URL

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function sendOTP() {
     message.innerText = "Sending OTP...";
 
     const url =
-        "https://electo-backend-xi.vercel.app/api/otp/send?email=" +
+        "https://electo-7p79.onrender.com/api/otp/send?email=" +
         encodeURIComponent(email);
 
     fetch(url, {
@@ -83,7 +83,7 @@ function verifyOTP() {
     message.innerText = "Verifying OTP...";
 
     const url =
-        "https://electo-backend-xi.vercel.app/api/otp/verify?email=" +
+        "https://electo-7p79.onrender.com/api/otp/verify?email=" +
         encodeURIComponent(email) +
         "&otp=" +
         encodeURIComponent(otp);
